cd9e1cc8c73d8ee1477ad6ac95dfae5f3e7cdcbf
Huzefa Saifee <huzefa.saifee@workday.com>
2024-03-06 06:55:18 -0700

    Added Navbar and Readjusted Contact Me Code.

---
+++ contact-me-section/js/main-code.js
@@ -0,0 +1,8 @@
+$(window).on("scroll", function() {
+    if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        document.getElementById("navbar_top").classList.add("header-fixed");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        document.getElementById("navbar_top").classList.remove("header-fixed");
+    }
+});
